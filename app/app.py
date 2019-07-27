fcf0541850708bd30a50dbdd608bbbd8477388a0
Dee <dee.rapunzel@gmail.com>
2019-07-27 21:04:50 +0700

    edit port

--- app/app.py
+++ app/app.py
@@ -25,4 +25,4 @@ def get_my_ip():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', threaded=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', threaded=True, port=7777)
